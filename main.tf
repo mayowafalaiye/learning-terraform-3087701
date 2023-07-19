0ee8b8b2b2d15967d1bf2fe0eff3abb7cdddacf1
mayowafalaiye <121531782+mayowafalaiye@users.noreply.github.com>
2023-07-19 01:42:28 -0400

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Define data block to retrieve the latest AWS AMI based on specified filters
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -14,8 +15,10 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+# Create an AWS VPC using the "terraform-aws-modules/vpc/aws" module
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "3.0.0"  # Use a specific version of the module for version pinning
 
   name = "dev"
   cidr = "10.0.0.0/16"
@@ -26,30 +29,50 @@ module "vpc" {
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
+# Launch an AWS EC2 instance using the specified AMI and instance type
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids  =  [module.blog_sg.security_group_id]
+  vpc_security_group_ids  = [module.blog_sg.security_group_id]
+
   tags = {
     Name = "HelloWorld"
   }
 }
 
+# Create an AWS security group using the "terraform-aws-modules/security-group/aws" module
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+  version = "5.1.0"  # Use a specific version of the module for version pinning
+
   name    = "blog"
 
-vpc_id  =  module.vpc.public_subnets[0]
+  vpc_id  = module.vpc.vpc_id  # Use the VPC ID from the created VPC module
 
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules = [
+    {
+      description      = "Allow HTTP inbound traffic"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    },
+    {
+      description      = "Allow HTTPS inbound traffic"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    },
+  ]
+
+  # For egress rules, it's not necessary to explicitly allow all traffic (all-all), 
+  # as all outbound traffic is allowed by default. We can omit the egress_rules and egress_cidr_blocks.
+  # If you want to restrict egress traffic, you can explicitly define the egress_rules as needed.
 }
