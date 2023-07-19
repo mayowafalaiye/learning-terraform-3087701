f31609c93c4f57231390cf8910988f6e1a1528a2
mayowafalaiye <121531782+mayowafalaiye@users.noreply.github.com>
2023-07-19 02:23:13 -0400

    l

--- main.tf
+++ main.tf
@@ -18,15 +18,13 @@ data "aws_ami" "app_ami" {
 # Create an AWS VPC using the "terraform-aws-modules/vpc/aws" module
 module "aws_default_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"  # Replace with a version that supports enable_classiclink
+  version = "3.0.0"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_classiclink = true  # Use the appropriate value for your use case
 
   enable_nat_gateway = true
 
