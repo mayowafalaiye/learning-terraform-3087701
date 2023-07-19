3d9bce202051cd24de119539e98289440e20e17c
mayowafalaiye <121531782+mayowafalaiye@users.noreply.github.com>
2023-07-18 23:09:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,6 +37,10 @@ resource "aws_security_group" "blog"  {
 
 resource "aws_security_group_rule" "blog_http_in"  {
   type         =  "ingress"
+  from_port    =  80
+  to_port      =  80
+  protocol     =  "tcp"
+  cidr_blocks  = ["0.0.0.0/0"]
 
   security_group_id  = aws_security_group.blog.id
 }
